5ff306b323d8316a6374a49eabd1d44a668fe8cc
Henrique Almeida <henrique.almeida.moura@gmail.com>
2020-01-25 23:56:58 -0300

    Apply webpack-server-dev in client-side

--- client/app-src/domain/negociacao/NegociacaoService.js
+++ client/app-src/domain/negociacao/NegociacaoService.js
@@ -12,7 +12,7 @@ export class NegociacaoService {
     obtemNegociacoesDaSemana() {
 
         return this._http
-            .get('negociacoes/semana')
+            .get('http://localhost:3000/negociacoes/semana')
             .then(
             dados =>
                 dados.map(objeto =>
@@ -43,7 +43,7 @@ export class NegociacaoService {
     obtemNegociacoesDaSemanaRetrasada() {
 
         return this._http
-            .get('negociacoes/retrasada')
+            .get('http://localhost:3000/negociacoes/retrasada')
             .then(
             dados => dados.map(objeto =>
                 new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
