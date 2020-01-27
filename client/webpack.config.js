843eff9ad83bb12a4dba3b5a139684914e9f59fe
Henrique Almeida <henrique.almeida.moura@gmail.com>
2020-01-27 20:51:16 -0300

    part scripts the package scripts

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,6 +14,12 @@ plugins.push(new webpack.ProvidePlugin({
     'jQuery': 'jquery/dist/jquery.js'
 }));
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}))
+
 if(process.env.NODE_ENV == 'production'){
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
     
@@ -30,7 +36,10 @@ if(process.env.NODE_ENV == 'production'){
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry:{
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    } ,
     output: {
         filename:'bundle.js',
         path: path.resolve(__dirname, 'dist'),
